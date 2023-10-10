22e518c8319c375f8633e6c9b45e121ce490b4ef
Grabarski Piotr <piotr.grabarski@exatel.pl>
2023-10-10 11:16:37 +0200

    Remove coc-jedi extension

--- coc/install_extensions.sh
+++ coc/install_extensions.sh
@@ -7,4 +7,4 @@ then
   echo '{"dependencies":{}}'> package.json
 fi
 
-npm install coc-cmake coc-css coc-diagnostic coc-elixir coc-fzf-preview coc-git coc-html coc-jedi coc-json coc-prettier coc-pyright coc-rust-analyzer coc-sh coc-tsserver coc-vetur coc-yaml --install-strategy=shallow --ignore-scripts --no-bin-links --no-package-lock --omit=dev
+npm install coc-cmake coc-css coc-diagnostic coc-elixir coc-fzf-preview coc-git coc-go coc-html coc-json coc-prettier coc-pyright coc-rust-analyzer coc-sh coc-tsserver coc-vetur coc-yaml --install-strategy=shallow --ignore-scripts --no-bin-links --no-package-lock --omit=dev
